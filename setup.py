5dbca103f1a42b1ea3d579ab8671ee529868afef
James Campbell <james@jamescampbell.us>
2016-08-02 16:46:07 -0400

    updates

--- setup.py
+++ setup.py
@@ -12,12 +12,12 @@ if version < '2.2.3':
 setup(
   name = 'spry',
   license='GNUv3',
-  version = '0.1.7',
+  version = '0.1.8',
   description = 'Spry is a social media collector toolsuite',
   author = 'James Campbell',
   author_email = 'james@jamescampbell.us',
   url = 'https://github.com/jamesacampbell/spry', # use the URL to the github repo
-  download_url = 'https://github.com/jamesacampbell/spry/tarball/0.1.7',
+  download_url = 'https://github.com/jamesacampbell/spry/tarball/0.1.8',
 
   keywords = ['testing', 'logging', 'example'], # arbitrary keywords
   classifiers=[
