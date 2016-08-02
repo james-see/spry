f17483aab0312cdfd42d35efa03bd4383372bbd7
James Campbell <james@jamescampbell.us>
2016-08-02 13:11:51 -0400

    first setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'spry',
+  packages = ['spry'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Spry is a social media collector toolsuite',
+  author = 'James Campbell',
+  author_email = 'james@jamescampbell.us',
+  url = 'https://github.com/peterldowns/mypackage', # use the URL to the github repo
+  download_url = 'https://github.com/jamesacampbell/spry/tarball/0.1', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  classifiers = [],
+)
