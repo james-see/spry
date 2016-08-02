641d42e6c2813a90b519b60938791fe942103ffd
James Campbell <james@jamescampbell.us>
2016-08-02 15:36:22 -0400

    added changes

--- setup.py
+++ setup.py
@@ -1,13 +1,21 @@
 from distutils.core import setup
+from sys import version
+if version < '2.2.3':
+    from distutils.dist import DistributionMetadata
+    DistributionMetadata.classifiers = None
+    DistributionMetadata.download_url = None
 setup(
   name = 'spry',
   packages = ['spry'], # this must be the same as the name above
-  version = '0.1',
+  version = '0.12',
   description = 'Spry is a social media collector toolsuite',
   author = 'James Campbell',
   author_email = 'james@jamescampbell.us',
-  url = 'https://github.com/peterldowns/mypackage', # use the URL to the github repo
-  download_url = 'https://github.com/jamesacampbell/spry/tarball/0.1', # I'll explain this in a second
+  url = 'https://github.com/jamesacampbell/spry', # use the URL to the github repo
+  download_url = 'https://github.com/jamesacampbell/spry/tarball/0.12', # I'll explain this in a second
   keywords = ['testing', 'logging', 'example'], # arbitrary keywords
-  classifiers = [],
+  classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Console',
+          'Programming Language :: Python',],
 )
