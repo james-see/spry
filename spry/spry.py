561ec0329d9550e435fc9456ccce0a89c04646c5
James Campbell <james@jamescampbell.us>
2016-08-03 12:51:17 -0400

    added requirements

--- spry/spry.py
+++ spry/spry.py
@@ -4,7 +4,7 @@
 """bootstrap.bootstrap: provides entry point main()."""
 
 
-__version__ = "0.3.8"
+__version__ = "0.3.9"
 # spry social media scanner
 #
 # Spry is free software: you can redistribute it and/or modify
