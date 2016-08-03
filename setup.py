c7e905be080fef2e9c983e4bfe641c9725e9cef6
jamesacampbell <james@jamescampbell.us>
2016-08-02 21:49:08 -0400

    updates to try to get command line working

--- setup.py
+++ setup.py
@@ -3,35 +3,35 @@ from codecs import open  # To use a consistent encoding
 from os import path
 
 here = path.abspath(path.dirname(__file__))
-
+def readme():
+    with open('README.md') as f:
+        return f.read()
 from sys import version
 if version < '2.2.3':
     from distutils.dist import DistributionMetadata
     DistributionMetadata.classifiers = None
     DistributionMetadata.download_url = None
 setup(
+  include_package_data=True,
   name = 'spry',
   license='GNUv3',
-  version = '0.1.8',
+  version = '0.1.9',
   description = 'Spry is a social media collector toolsuite',
+  long_description = readme(),
   author = 'James Campbell',
   author_email = 'james@jamescampbell.us',
   url = 'https://github.com/jamesacampbell/spry', # use the URL to the github repo
   download_url = 'https://github.com/jamesacampbell/spry/tarball/0.1.8',
 
-  keywords = ['testing', 'logging', 'example'], # arbitrary keywords
+  keywords = ['social', 'collector', 'scraper'], # arbitrary keywords
   classifiers=[
           'Development Status :: 4 - Beta',
           'Environment :: Console',
           'Programming Language :: Python',],
       packages=["spry", "spry.modules"],
     package_data={'spry': ['sociallist/sociallist.txt']},
-  install_requires = ['requests>=1.0'],
-
+  install_requires = ['requests>=1.0',],
     entry_points={
-        'console_scripts': [
-            'spry=spry.spry:main',
-        ],
-    },
-
+        'console_scripts': [ 'spry=spry.spry:main',],
+        },
 )
