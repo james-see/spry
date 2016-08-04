a78d5076b6fe2935ec9b65cae49a31265fb9c920
jamesacampbell <james@jamescampbell.us>
2016-08-03 23:00:53 -0400

    moare

--- spry/spry.py
+++ spry/spry.py
@@ -4,7 +4,7 @@
 """bootstrap.bootstrap: provides entry point main()."""
 
 
-__version__ = "0.4.2"
+__version__ = "0.4.3"
 # spry social media scanner
 #
 # Spry is free software: you can redistribute it and/or modify
