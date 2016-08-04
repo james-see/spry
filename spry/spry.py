2827fa1beeb1363cee0a5a2ab4c602bd19fa221c
James Campbell <james@jamescampbell.us>
2016-08-04 10:46:45 -0400

    updates to readme and version

--- spry/spry.py
+++ spry/spry.py
@@ -4,7 +4,7 @@
 """bootstrap.bootstrap: provides entry point main()."""
 
 
-__version__ = "0.4.7"
+__version__ = "0.4.8"
 # spry social media scanner
 #
 # Spry is free software: you can redistribute it and/or modify
