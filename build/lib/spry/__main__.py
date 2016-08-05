ad54ac8a3d87cec6a248445cbb24f236b77ec80d
James Campbell <james@jamescampbell.us>
2016-08-05 11:03:12 -0400

    more fixes

--- build/lib/spry/__main__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-
-"""spry.__main__: executed when spry directory is called as script."""
-
-
-from .spry import main
-main()
--- build/lib/spry/modules/stuff.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-
-"""spry.stuff: stuff module within the bootstrap package."""
-
-
-class Stuff(object):
-    pass
